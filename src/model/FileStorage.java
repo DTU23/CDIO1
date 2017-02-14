8edadf3b3a88ccb3915d706ec7bd24562c24acb0
Frederik Værnegaard <frederik@nyka.dk>
2017-02-14 16:57:49 +0100

    Updated FileStorage, but not done
    Need to redesign methods

--- src/model/FileStorage.java
+++ src/model/FileStorage.java
@@ -1,11 +1,15 @@
 package model;
 
+import java.io.File;
 import java.util.ArrayList;
 
-/**
- * Created by Christian on 14/02/2017.
- */
 public class FileStorage implements IDataStorage {
+
+    private String path;
+
+    public FileStorage(String path) {
+        this.path = path;
+    }
 
     @Override
     public boolean write(ArrayList<UserDTO> users) {
@@ -16,4 +20,78 @@ public class FileStorage implements IDataStorage {
     public ArrayList<UserDTO> read() {
         return null;
     }
+
+    public boolean fileExists() {
+		return new File(path).exists();
+	}
+
+	public void createFile() {
+		try{
+			PrintWriter writer = new PrintWriter("the-file-name.txt", "UTF-8");
+			writer.println("");
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private UserDAO loadUsers() throws DALException {
+		UserDAO userStore = new UserDAO(path);
+		ObjectInputStream oIS = null;
+
+		try {
+
+			FileInputStream fIS = new FileInputStream(path);
+			oIS = new ObjectInputStream(fIS);
+			Object inObj = oIS.readObject();
+
+			if (inObj instanceof UserDAO){
+				userStore = (UserDAO) inObj;
+			} else {
+				throw new DALException("Wrong object in file");
+			}
+
+		} catch (FileNotFoundException e) {
+			//No problem - just returning empty userstore
+		} catch (IOException e) {
+			throw new DALException("Error while reading disk!", e);
+		} catch (ClassNotFoundException e) {
+			throw new DALException("Error while reading file - Class not found!", e);
+		} finally {
+			if (oIS!=null){
+				try {
+					oIS.close();
+				} catch (IOException e) {
+					throw new DALException("Error closing pObjectStream!", e);
+				}
+			}
+		}
+
+		return userStore;
+	}
+
+	private void saveUsers(UserDAO users) throws DALException {
+		ObjectOutputStream oOS = null;
+
+		try {
+
+			FileOutputStream fOS = new FileOutputStream(path);
+			oOS = new ObjectOutputStream(fOS);
+			oOS.writeObject(users);
+
+		} catch (FileNotFoundException e) {
+			throw new DALException("Error locating file", e);
+		} catch (IOException e) {
+			throw new DALException("Error writing to disk", e);
+		} finally {
+			if (oOS!=null) {
+				try {
+					oOS.close();
+				} catch (IOException e) {
+					throw new DALException("Unable to close ObjectStream", e);
+				}
+			}
+		}
+	}
+
 }

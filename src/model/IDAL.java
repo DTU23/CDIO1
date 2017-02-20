f6b0f183ae42e7510abd3ebbe9bf301636f368aa
Christian Niemann <spirem@gmail.com>
2017-02-20 19:24:06 +0100

    updated structure

---
+++ src/model/IDAL.java
@@ -0,0 +1,22 @@
+package model;
+
+import java.util.ArrayList;
+
+import model.IDataStorage.DALException;
+
+public interface IDAL {
+
+	UserDTO getUser(int userId) throws DALException;
+
+	ArrayList<UserDTO> getUserList();
+	
+	boolean isUserListEmpty();
+
+	void createUser(UserDTO user) throws DALException;
+
+	void updateUser(UserDTO user) throws DALException;
+
+	void deleteUser(int userId) throws DALException;
+
+	boolean userExists(int userId);
+}

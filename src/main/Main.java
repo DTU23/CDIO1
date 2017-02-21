62b985079dd603e7e4d8a7d9d8259834a47a2485
Frederik Værnegaard <frederik@nyka.dk>
2017-02-21 08:49:18 +0100

    Fixed Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,15 +1,13 @@
 package main;
 import control.Ctrl;
-import model.FileStorage;
-import model.IDataStorage;
-import model.UserDAO;
+import model.*;
 import view.TUI;
 
 public class Main{
 	
     public static void main(String[] args){
         IDataStorage storage = new FileStorage();
-        UserDAO dao = new UserDAO(storage);
+        IDAL dao = new PersistentUserDAO(storage);
         Ctrl controller = new Ctrl(dao);
         TUI tui = new TUI(controller);
         tui.run();
